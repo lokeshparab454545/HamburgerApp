28c0632b91d2c4574d05c570a8c380f1403766fd
Lokesh Parab <lokeshparab4545@gmail.com>
2022-06-25 15:02:19 +0530

    day1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:burger/header.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(color: Colors.teal),
+        bottomAppBarColor: Colors.teal,
+        floatingActionButtonTheme:
+            const FloatingActionButtonThemeData(backgroundColor: Colors.amber),
+      ),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
@@ -27,6 +34,67 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            centerTitle: true,
+            title: const Text("Deliver Me"),
+            leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.shopping_cart),
+              ),
+            ],
+          ),
+          const Header(),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                const Text(
+                  "Hamburger",
+                  style: TextStyle(fontSize: 300),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+      extendBody: true,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.home),
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(45)),
+        child: Container(
+          color: Colors.black38,
+          child: BottomAppBar(
+            shape: const CircularNotchedRectangle(),
+            child: Row(
+              children: [
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add_alert),
+                  color: Colors.white,
+                ),
+                const Spacer(),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.turned_in),
+                  color: Colors.white,
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
